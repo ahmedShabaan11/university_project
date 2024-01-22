8c764b80c3503c7523393fb576f011d88e3b9311
ADMIN <ma2155151@gmail.com>
2024-01-22 19:24:19 +0200

    f

--- lib/screens/chat_screen/chat_screen.dart
+++ lib/screens/chat_screen/chat_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:university/constants.dart';
 import 'package:chat_bubbles/chat_bubbles.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:university/screens/chat_screen/chat_layout.dart';
 import '../../componenets/chat_bubble_model.dart';
 
 class Chat_Screen extends StatelessWidget {
@@ -15,64 +16,18 @@ class Chat_Screen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<DocumentSnapshot>(
-      future: messages.doc('1f9xhEwtcOJ9lseczZl0').get(),
-      builder: (context, snapshot) {
-        snapshot.data;
-        return Scaffold(
-            appBar: AppBar(
-              automaticallyImplyLeading: false,
-              backgroundColor: kPrimaryColor,
-              title: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    'assets/images/scholar.png',
-                    height: 50,
-                  ),
-                  const Text('Chat'),
-                ],
-              ),
-            ),
-            body: Column(
-              children: [
-                Expanded(
-                  child: ListView.builder(itemBuilder: (context, index) {
-                    return const Caht_Bubble();
-                  }),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: TextField(
-                    controller: controller,
-                    onSubmitted: (data) {
-                      messages.add({
-                        'message': data,
-                      });
-                      controller.clear();
-                    },
-                    decoration: InputDecoration(
-                      hintText: 'Sent Message',
-                      hintStyle: const TextStyle(color: Colors.grey),
-                      suffixIcon: const Icon(
-                        Icons.send,
+    return FutureBuilder<QuerySnapshot>(
+        future: messages.get(),
+        builder: (context, snapshot) {
+          snapshot.data;
+          if (snapshot.hasError) { return ChatLayout();
 
-                        color: Colors.white,
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15),
-                          borderSide: const BorderSide(color: kPrimaryColor)),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(16),
-                        borderSide: const BorderSide(color: kPrimaryColor),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            backgroundColor: kContainerColor);
-      },
-    );
+          }else {const Text('Loading....');}
+
+          if (snapshot.hasData && !snapshot.data!.exists) {
+            return Text("Document does not exist");
+          }
+
+        });
   }
 }
